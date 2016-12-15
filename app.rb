a4d7594a641bb89fb566b1df6cc6e58e6410ecbe
Armando Fox <fox@cs.berkeley.edu>
2016-12-15 11:13:52 -0800

    disable sticky application error if user/pass not found

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class Audience1stReboot < Sinatra::Base
   if ENV['RACK_ENV'] == 'production'
     use Rack::SSL
     use Rack::Auth::Basic, "Restricted Area" do |username, password|
-      if Figaro.env.send("#{username}_password!") == password
+      if Figaro.env.send("#{username}_password") == password
         Rollbar.info "Successful login by #{username}"
         set :user, username.capitalize
       else
@@ -56,13 +56,12 @@ class Audience1stReboot < Sinatra::Base
       @server.reboot('SOFT')
       Rollbar.info "Successful reboot"
       erb :result
-    rescue StandardError, Fog::Rackspace::Errors::BadRequest, Fog::Rackspace::Errors::Conflict,
+    rescue StandardError, Exception,
+      Fog::Rackspace::Errors::BadRequest, Fog::Rackspace::Errors::Conflict,
       Fog::Rackspace::Errors::InternalServerError, Fog::Rackspace::Errors::MethodNotAllowed,
       Fog::Rackspace::Errors::ServiceError, Fog::Rackspace::Errors::ServiceUnavailable => @e
       Rollbar.error @e
       erb :result
-    rescue Exception => e       # other unexpected error
-      Rollbar.error(e)
     end
   end
 end
